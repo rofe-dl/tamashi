4c5a0330405955f0cec93f03598358a327eefd75
rofe-dl <rafidul258@gmail.com>
2023-02-19 18:26:18 +0600

    feat: authorization reply tells the user about giving their email address

--- utils/enums/replies.js
+++ utils/enums/replies.js
@@ -1,6 +1,6 @@
 module.exports = {
   SONG_NOT_FOUND: "I couldn't find any song by that name!",
-  VISIT_AUTHORIZE_URL: `It seems you have not authorized me to follow your Spotify yet. Please visit the following link and grant access to me so I can track what you're listening to. You can revoke this anytime you want from Spotify account settings. After authorizing, try the command again!\n`,
+  VISIT_AUTHORIZE_URL: `It seems you have not authorized me to follow your Spotify yet. Please visit the following link and grant access to me so I can track what you're listening to. After authorizing, give the bot host the email you used for your Spotify. Then, try the command again!\n`,
   ACCESS_REVOKED: `I no longer have permission to access your Spotify data. In case you revoked it and want to reauthorize me, use the \`/forgetme\` command to reset yourself first and then try again!`,
   FORGOT_YOU: `I have successfully forgotten who you are. If you want to reauthorize me again, use the \`/followme\` command.`,
   CANT_FORGET_USER_I_DK: `I couldn't find you in my database, so I can't exactly forget someone I don't even know.`,
