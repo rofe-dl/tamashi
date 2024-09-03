3e9d95d2286b010208fb2df3921ef0c0bc60c759
rofe-dl <rafidul258@gmail.com>
2024-09-03 16:10:38 +0600

    refactor: deleted old codebase

--- utils/regex.js
+++
@@ -1,8 +0,0 @@
-const HTTP_URL_REGEX =
-  /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
-const URL_REGEX =
-  /^[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/;
-
-module.exports.isURL = (s) => {
-  return HTTP_URL_REGEX.test(s) || URL_REGEX.test(s);
-};
