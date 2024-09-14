61e40becf00fdac453aa897a4e49d48b81b1944c
rofe-dl <rafidul258@gmail.com>
2024-09-14 08:27:51 +0600

    feat: refactored redis class and added delete function

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,67 +1,91 @@
 import { createClient, RedisClientType } from 'redis';
 import logger from './logger';
-
+import { redis } from 'config.json';
 
 class RedisClient {
-    private static instance: RedisClient; // Singleton instance
-    private client: RedisClientType;
-    private isConnected: boolean = false;
+  private static instance: RedisClient; // Singleton instance
+  private client: RedisClientType;
+  private isConnected: boolean = false;
 
-    private constructor(url: string) {
-        this.client = createClient({ url });
-        this.client.on('error', (err) => logger.error('Redis Client Error', err));
+  private constructor() {
+    this.client = createClient({
+      url: `redis://${redis.host}:${redis.port}`,
+      socket: { reconnectStrategy: false },
+    });
+    this.client.on('error', (err) => logger.error('Redis Client Error', err));
+  }
+
+  static getInstance(): RedisClient {
+    if (!RedisClient.instance) {
+      RedisClient.instance = new RedisClient();
     }
+    return RedisClient.instance;
+  }
 
-    static getInstance(url: string): RedisClient {
-        if (!RedisClient.instance) {
-            RedisClient.instance = new RedisClient(url);
-        }
-        return RedisClient.instance;
+  public getClient(): RedisClientType {
+    return this.client;
+  }
+
+  async connect(): Promise<void> {
+    if (!this.isConnected) {
+      await this.client.connect();
+      this.isConnected = true;
+    } else {
+      logger.info('Redis is already connected.');
     }
+  }
 
-    public getClient(): RedisClientType { return this.client; }
-
-    // Connect to Redis
-    async connect(): Promise<void> {
-        if (!this.isConnected) {
-            await this.client.connect();
-            this.isConnected = true;
-            logger.info("Connected to Redis.");
-        } else {
-            logger.info("Redis is already connected.");
-        }
+  async disconnect(): Promise<void> {
+    if (this.isConnected) {
+      await this.client.disconnect();
+      this.isConnected = false;
+      logger.info('Disconnected from Redis.');
     }
+  }
 
-    // Disconnect from Redis
-    async disconnect(): Promise<void> {
-        if (this.isConnected) {
-            await this.client.disconnect();
-            this.isConnected = false;
-            logger.info("Disconnected from Redis.");
-        }
+  async flush(): Promise<void> {
+    try {
+      await this.client.flushAll();
+    } catch (err) {
+      logger.error('Error flushing redis: ' + err);
     }
+  }
 
-    // Set key-value pair
-    async set(key: string, value: string): Promise<void> {
-        try {
-            await this.client.set(key, value);
-            logger.info(`Set key ${key} to ${value} successfully.`);
-        } catch (error) {
-            logger.error(`Error setting key ${key}:`, error);
-        }
-    }
+  async set(key: string | null, value: string | null): Promise<void> {
+    try {
+      if (!key || !value) throw new Error('Cannot set key/value as null');
 
-    // Get value by key
-    async get(key: string): Promise<string | null> {
-        try {
-            const value = await this.client.get(key);
-            logger.info(`Retrieved key ${key} successfully.`);
-            return value;
-        } catch (error) {
-            logger.error(`Error getting key ${key}:`, error);
-            return null;
-        }
+      await this.client.set(key, value);
+      logger.info(`Set key ${key} to ${value} successfully.`);
+    } catch (error) {
+      logger.error(`Error setting key ${key}:`, error);
     }
+  }
+
+  async get(key: string | null): Promise<string | null> {
+    try {
+      if (key == null) return null;
+
+      const value = await this.client.get(key);
+      logger.info(`Retrieved key ${key} successfully.`);
+
+      return value;
+    } catch (error) {
+      logger.error(`Error getting key ${key}:`, error);
+      return null;
+    }
+  }
+
+  async delete(key: string | null): Promise<void> {
+    try {
+      if (key == null) throw new Error('Cannot remove key as null');
+
+      await this.client.del(key);
+      logger.info(`Removed key ${key} successfully.`);
+    } catch (error) {
+      logger.error(`Error removing key ${key}:`, error);
+    }
+  }
 }
 
 export default RedisClient;
