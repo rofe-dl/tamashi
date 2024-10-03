682b72edd91d8b42ce855be8a89951639f8de049
rofe-dl <rafidul258@gmail.com>
2024-10-03 19:51:58 +0600

    fix: user can retry commands to make bot switch channels

--- src/commands/spotify/unfollowme.ts
+++ src/commands/spotify/unfollowme.ts
@@ -8,6 +8,17 @@ export default {
     .setDescription('Unfollows your Spotify and stops playing'),
 
   async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    const player = interaction.client?.shoukaku?.players?.get(
+      interaction.guildId as string,
+    );
+
+    /**
+     * Removes listeners as we're not doing a force disconnect.
+     * We don't wanna trigger the listener on 'closed' event
+     * as that is intended for forced disconnects only.
+     */
+    player?.clean();
+
     await Promise.all([
       interaction.client?.shoukaku?.leaveVoiceChannel(interaction.guildId as string),
       RedisClient.getInstance().delete(interaction.guildId),
